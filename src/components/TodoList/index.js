ea5838d0a753405c85755ce6667e76b8973b8bc7
范轶洁 <fanyjb@digitalchina.com>
2020-05-21 18:01:09 +0800

    local storage

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 
 export default function ToDoList() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const lsTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(lsTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const onAddNewTodo = (title) => {
     setTodos((preTodos) => [
